eff8da2665c81eb53af84cbaadd1d8d152ed2685
leonardosousa <leodgsousa@gmail.com>
2023-12-08 01:58:55 +0000

    Commented code

--- src/main/java/uc/mei/is/FlowHandler.java
+++ src/main/java/uc/mei/is/FlowHandler.java
@@ -9,39 +9,32 @@ import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uc.mei.is.models.AverageSum;
-
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.Random;
 
-import static java.lang.ProcessBuilder.Redirect.to;
-
 public class FlowHandler {
     private static final Logger log = LoggerFactory.getLogger(FlowHandler.class);
 
     public static void main(String[] args) {
+
+        //setting the 3 brokers available
         String bootstrapServers = "127.0.0.1:29092,127.0.0.1:29093,127.0.0.1:29094";
+
+        //input topics
         String inputTopic_sales = "sock_sales_topic";
         String inputTopic_purchases = "sock_purchases_topic";
-        String outputTopic = "result_topic";
 
+        //defining properties
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "flow_handler");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        Random rand = new Random();
 
-
-        // Get from a Topic
-        StreamsBuilder builder = new StreamsBuilder();
-
-        // STRING SALE RECEBIDA:
+        // STRING SALE RECEIVED FROM INPUT TOPICS:
         // TAM: 6
         // 0 -> SOCK ID
         // 1 -> SUPPLIER ID
@@ -50,7 +43,10 @@ public class FlowHandler {
         // 4 -> SALE/PURCHASE PRICE
         // 5 -> QUANTITY
 
-        //stream setup
+        //streams setup
+        StreamsBuilder builder = new StreamsBuilder();
+
+        // sales stream , id -> ref da meia
         KStream<Integer, String> salesOrders = builder.stream(inputTopic_sales)
                 .filter((key, value) -> String.valueOf(value).split(";").length == 6)
                 .groupBy((key, value) -> Integer.valueOf(String.valueOf(value).split(";")[0]))
@@ -62,7 +58,7 @@ public class FlowHandler {
                 ).mapValues((values) -> String.valueOf(values))
                 .toStream();
 
-
+        // purchases stream, id -> ref da meia
         KStream<Integer, String> purchasesOrders = builder.stream(inputTopic_purchases).filter((key, value) -> String.valueOf(value).split(";").length == 6)
                 .groupBy((key, value) -> Integer.valueOf(String.valueOf(value).split(";")[0]))
                 .aggregate(
@@ -73,33 +69,33 @@ public class FlowHandler {
                 ).mapValues((values) -> String.valueOf(values))
                 .toStream();
 
+        // joined stream (sales vs purchases)
         KStream<Integer, String> joinedOrders = salesOrders
                 .join(purchasesOrders,
                         (leftValue, rightValue) -> leftValue + ';' + rightValue,
                         JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofSeconds(5)));
 
-        // STREAMS FOR SOCK TYPE
-        /*KStream<Integer, String> purchasesOrdersTypes = builder.stream(inputTopic_sales)
-                .filter((key, value) -> String.valueOf(value).split(";").length == 6)
-                .groupBy((key, value) -> String.valueOf(value).split(";")[3])
-                mapValues((values) -> String.valueOf(values))
-                .toStream();
-        */
 
 
-        //Requirements
+        //Requirements --------------------------------------------------------------------
+
+
+
         //5. Get the revenue per sock pair sale.
+        // Faz um map dos valores para colocar o vendedor, tipo de meia e valor total da venda
         salesOrders
                 .mapValues((key, value) -> value.split(";")[2] + '-' + value.split(";")[3] + '[' + key + "] teve uma venda no valor de " + String.format(java.util.Locale.US, "%.2f", Float.parseFloat(value.split(";")[4]) * Float.parseFloat(value.split(";")[5])) + "€")
                 .to("topic-5");
 
         //6. Get the expenses per sock pair sale.
+        // Faz um map dos valores para colocar de onde foi comprado, tipo de meia e valor total da compra
         purchasesOrders
                 .mapValues((key, value) -> value.split(";")[2] + '-' + value.split(";")[3] + '[' + key + "] foi realizada uma compra no valor de " + String.format(java.util.Locale.US, "%.2f", Float.parseFloat(value.split(";")[4]) * Float.parseFloat(value.split(";")[5])) + "€")
                 .to("topic-6");
 
 
         //7. Get the profit per sock pair sale.
+        // Faz um map dos valores para verificar o lucro em si (ganhos-gastos)
         joinedOrders
                 .filter((key, value) -> value.split(";").length == 12)
                 .mapValues((key, value) -> String.format(java.util.Locale.US, "%s : %.2f€", value.split(";")[2] + '-' + value.split(";")[3] + '[' + key + "] teve um lucro de ", ((Float.valueOf(String.valueOf(value).split(";")[4]) * Float.valueOf(String.valueOf(value).split(";")[5])) - (Float.valueOf(String.valueOf(value).split(";")[10]) * Float.valueOf(String.valueOf(value).split(";")[11])))))
@@ -107,6 +103,7 @@ public class FlowHandler {
 
 
         //8. Get the total revenues.
+        // Faz um map dos valores para obter ganhos totais, junta todos no mesmo grupo e junta com o já existente.
         salesOrders
                 .mapValues((value) -> String.valueOf(Float.parseFloat(value.split(";")[4]) * Integer.parseInt(value.split(";")[5])))
                 .groupBy((key, value) -> 100)
@@ -116,6 +113,7 @@ public class FlowHandler {
 
 
         //9. Get the total expenses.
+        // Faz um map dos valores para obter gastos totais, junta todos no mesmo grupo e junta com o já existente.
         purchasesOrders
                 .mapValues((value) -> String.valueOf(Float.parseFloat(value.split(";")[4]) * Integer.parseInt(value.split(";")[5])))
                 .groupBy((key, value) -> 100)
@@ -125,6 +123,7 @@ public class FlowHandler {
 
 
         //10. Get the total profit.
+        // Faz um map dos valores para obter lucros (ganhos-gastos), junta todos no mesmo grupo e junta com o já existente.
         joinedOrders
                 .mapValues((value) -> String.valueOf((Float.parseFloat(value.split(";")[4]) * Integer.parseInt(value.split(";")[5])) - (Float.parseFloat(value.split(";")[10]) * Integer.parseInt(value.split(";")[11]))))
                 .groupBy((key, value) -> 100)
@@ -133,6 +132,8 @@ public class FlowHandler {
                 .to("topic-10");
 
         //11. Get the average amount spent in each purchase (separated by sock type).
+        // Junta todas as meias que tiverem o mesmo tipo, para cada grupo junta o nome do tipo, quantidade total e
+        // gastos totais, faz map dos valores para organizar a informação.
         purchasesOrders
                 .groupBy((key, value) -> convertStringToNumber(value.split(";")[3]))
                 .aggregate(
@@ -146,6 +147,8 @@ public class FlowHandler {
 
 
         //12. Get the average amount spent in each purchase (aggregated for all socks).
+        // Junta todas as meias, agrega a quantidade total e
+        // gastos totais, faz map dos valores para organizar a informação.
         purchasesOrders
                 .groupBy((key, value) -> 100)
                 .aggregate(
@@ -158,6 +161,10 @@ public class FlowHandler {
                 .toStream().to("topic-12");
 
         //13. Get the sock type with the highest profit of all (only one if there is a tie).
+        // Junta todas as meias que teem o mesmo tipo, para cada grupo junta o lucro e o nome
+        // do tipo de meia, junta todas para conseguir encontrar a que teve maior lucro,
+        // faz map dos valores para personalizar o texto
+
         joinedOrders
                 .groupBy((key, value) -> convertStringToNumber(value.split(";")[3]))
                 .aggregate(
@@ -182,6 +189,9 @@ public class FlowHandler {
 
 
         //14. Get the total revenue in the last hour1 (use a tumbling time window).
+        // Map dos valores para ter o ganho, junta todas as meias no mesmo grupo
+        // aplica uma tumbling de 1 hora e junta os valores
+
         Duration windowDurationSales = Duration.ofSeconds(120);
         TimeWindows tumbWindowSales = TimeWindows.ofSizeWithNoGrace(windowDurationSales);
 
@@ -195,6 +205,9 @@ public class FlowHandler {
                 .to("topic-14");
 
         //15. Get the total expenses in the last hour (use a tumbling time window).
+        // Map dos valores para ter o gasto, junta todas as meias no mesmo grupo
+        // aplica uma tumbling de 1 hora e junta os valores
+
         Duration windowDurationPurchases = Duration.ofSeconds(120);
         TimeWindows tumbWindowPurchases = TimeWindows.ofSizeWithNoGrace(windowDurationPurchases);
 
@@ -208,6 +221,9 @@ public class FlowHandler {
                 .to("topic-15");
 
         //16. Get the total profits in the last hour (use a tumbling time window).
+        // Map dos valores para ter o lucro, junta todas as meias no mesmo grupo
+        // aplica uma tumbling de 1 hora e junta os valores
+
         Duration windowDurationProfit = Duration.ofSeconds(120);
         TimeWindows tumbWindowProfit = TimeWindows.ofSizeWithNoGrace(windowDurationProfit);
         joinedOrders
@@ -220,6 +236,10 @@ public class FlowHandler {
                 .to("topic-16");
 
         //17. Get the name of the sock supplier generating the highest profit sales. Include the value of such sales.
+        // Junta as meias que veem do mesmo supplier, para cada grupo junta o lucro e o nome
+        // do supplier, junta todas para conseguir encontrar o supplier que teve maior lucro,
+        // faz map dos valores para personalizar o texto
+
         joinedOrders
                 .groupBy((key, value) -> Integer.valueOf(value.split(";")[1]))
                 .aggregate(
@@ -243,14 +263,13 @@ public class FlowHandler {
                 .toStream().to("topic-17");
 
 
-
-
         // start kafkaStreams
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.start();
 
     }
 
+    // transforma uma string num número único
     private static int convertStringToNumber(String str) {
         int result = 0;
         for (int i = 0; i < str.length(); i++) {
